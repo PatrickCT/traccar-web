6f6585e30d54769fe5d0a9183bc5dcc890d23cba
PatrickCT <70776685+PatrickCT@users.noreply.github.com>
2024-02-02 16:31:07 -0600

    Update DebtModal.js

--- modern/src/common/components/DebtModal.js
+++ modern/src/common/components/DebtModal.js
@@ -1,14 +1,29 @@
 // import { Button } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 // const DebtModal = ({ onClose }) => {
 const DebtModal = () => {
+  const userId = useSelector((state) => state.session.user.id);
+
+  let lh = 0.7;
+  let f = 50;
+  let w = 80;
+  let h = 80;
+
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    h = 90;
+    f = 35;
+    lh = 0.9;
+    w = 95;
+  }
+
   const modalOverlayStyle = {
     position: 'fixed',
     top: 0,
     left: 0,
     width: '100%',
     height: '100%',
-    background: 'rgba(0, 0, 0, 0.5)',
+    background: 'rgba(0, 0, 0, 0)',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
@@ -18,45 +33,83 @@ const DebtModal = () => {
   const modalContentStyle = {
     background: '#fff',
     padding: '20px',
-    borderRadius: '5px',
-    width: '80%',
-    maxWidth: '80%',
+    borderRadius: '25px',
+    width: `${w}%`,
+    height: `${h}%`,
     boxShadow: '0 2px 5px rgba(0, 0, 0, 0.2)',
-
-  };
-
-  const modalHeaderStyle = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: '10px',
+    backgroundColor: 'rgb(40 72 113 / 80%)',
+    fontSize: f,
+    lineHeight: lh,
   };
 
   const modalBodyStyle = {
-    marginBottom: '20px',
-    fontSize: 30,
+    fontSize: f,
   };
 
-  const modalFooterStyle = {
-    display: 'flex',
-    justifyContent: 'flex-end',
+  const paragraphStyle = {
+    textAlign: 'center',
+    fontSize: `${f}px`,
+    lineHeight: `${lh}`,
   };
+
+  const [result, setResult] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://45.79.45.108:4040/api/external/${userId}`)
+      .then((response) => response.json())
+      .then((data) => setResult(data));
+  }, []);
 
   return (
     <div style={modalOverlayStyle}>
       <div style={modalContentStyle}>
-        <div style={modalHeaderStyle}>
-          <h1>Recordatorio de pagos pendientes</h1>
-          {/* You can add a close button or any other elements as needed */}
-        </div>
         <div style={modalBodyStyle}>
-          <p>
-            Usted tiene un saldo pendiente, para continuar usando la plataforma le pedimos que realice su pago, si necesita una aclaración comuniquece con soporte
+          <p style={paragraphStyle}>
+            <span style={{ color: '#ffffff' }}>
+              <strong>SU CUENTA PRESENTA UN PAGO PENDIENTE</strong>
+            </span>
           </p>
-        </div>
-        <div style={modalFooterStyle}>
-          {/* You can add buttons or links for actions, such as making a payment */}
-          {/* <Button onClick={onClose} /> */}
+          <p style={paragraphStyle}>
+            <span style={{ color: '#000000' }}>
+              <strong>IMPORTE: </strong>
+            </span>
+            <span style={{ color: '#ffffff' }}>$</span>
+            <span style={{ color: '#ffffff' }}>{result?.data?.debt}</span>
+          </p>
+          <p style={paragraphStyle}>
+            <span style={{ color: '#ff0000' }}>
+              <strong>PAGUE A TRAV&Eacute;Z DE:</strong>
+            </span>
+          </p>
+          <p style={paragraphStyle}>
+            <span style={{ color: '#000000' }}>
+              <strong>TRANSFERENCIA</strong>
+            </span>
+          </p>
+          <p style={paragraphStyle}>
+            <span style={{ color: '#ffffff' }}>
+              <strong>{result?.data?.stp}</strong>
+            </span>
+          </p>
+          <p style={paragraphStyle}>
+            <span style={{ color: '#000000' }}>
+              <strong>LINK DE PAGO</strong>
+            </span>
+          </p>
+          <p style={paragraphStyle}>
+            <a style={{ color: '#ffffff' }} href="https://clip.mx/@gpstrackermx" target="_blank" rel="noreferrer">
+              <strong>https://clip.mx/@gpstrackermx</strong>
+            </a>
+          </p>
+          <p style={paragraphStyle}>
+            <span style={{ color: '#ffffff' }}>
+              <strong>
+                MAYORES INFORMES:
+                <a style={{ color: 'inherit' }} href="tel:4434521162"> 4434521162 </a>
+                EXT. 3
+              </strong>
+            </span>
+          </p>
         </div>
       </div>
     </div>
